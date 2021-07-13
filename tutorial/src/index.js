58d3fef20bfca75b0a717f91da51c5d4df2d2595
Kim Wheaton <kwheatonart@gmail.com>
2021-07-13 17:16:36 -0400

    class ball added

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,5 +1,6 @@
-import Paddle from "/src/paddle";
-import InputHandler from "/src/input";
+import Paddle from "./paddle.js";
+import InputHandler from "./input.js";
+import Ball from "./ball.js";
 
 let canvas = document.getElementById("gameScreen");
 
@@ -8,29 +9,33 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0, 0, 800, 600);
+// ctx.clearRect(0, 0, 800, 600);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
 
 new InputHandler(paddle);
 
-paddle.draw(ctx);
-
 let lastTime = 0;
+
+// paddle.draw(ctx);
+
 
 function gameLoop(timestamp) {
 
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
-    ctx.clearRect(0, 0, 800, 600);
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
     paddle.update(deltaTime);
-
     paddle.draw(ctx);
+
+    ball.update(deltaTime);
+    ball.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
--- tutorial/src/paddle.js
+++ tutorial/src/paddle.js
@@ -2,7 +2,7 @@ export default class Paddle {
 
     constructor (gameWidth, gameHeight) {
         this.gameWidth = game.gameWidth;
-        
+
         this.width = 150;
         this.height = 20;
 
@@ -34,8 +34,6 @@ export default class Paddle {
 
     update(deltaTime) {
 
-        if(!deltaTime) return;
-
         this.position.x += this.speed;
 
         if (this.position.x < 0) this.position.x = 0;
@@ -44,3 +42,18 @@ export default class Paddle {
             this.position.x = this.gameWidth - this.width;
     }
 }
+
+
+// constructor(game){
+//     this.gameWidth = game.gameWidth;
+
+//     this.width = 150;
+//     this.height = 20;
+    
+//     this.maxSpeed = 7;
+//     this.speed = 0;
+
+//     this.position = {
+//         x: game.gameWidth /  2 - this.width / 2,
+//         y: game.gameHeight - this.height -10,
+//     };
