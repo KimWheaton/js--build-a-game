c6e3919b8286509f38079fa1953a2c9626b2e83e
Kim Wheaton <kwheatonart@gmail.com>
2021-07-14 14:28:07 -0400

    working brick breaker tutorial

--- tutorial/src/ball.js
+++ tutorial/src/ball.js
@@ -30,12 +30,18 @@ export default class Ball {
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
-        // wall on top
-        if(this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+        // wall on left or right
+        if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
             this.speed.x = -this.speed.x;
         }
+
+        // wall on top
+        if (this.position.y < 0) {
+            this.speed.y = -this.speed.y;
+        }
+
         // bottom of game
-        if(this.position.y + this.size > this.gameHeight) {
+        if (this.position.y + this.size > this.gameHeight) {
             this.game.lives -- ;
             this.reset();  
         } 
